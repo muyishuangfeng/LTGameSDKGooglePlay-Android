2ee48666af9de84c4163ef08702249f804e2be75
silence <yangkemuyi@sina.com>
2019-07-12 14:57:34 +0800

    first commit

---
+++ ltgamegoogleplay/src/main/java/com/gentop/ltgame/ltgamegoogleplay/uikit/GooglePlayActivity.java
@@ -0,0 +1,9 @@
+package com.gentop.ltgame.ltgamegoogleplay.uikit;
+
+
+import com.gentop.ltgame.ltgamesdkcore.uikit.BaseActionActivity;
+
+public class GooglePlayActivity extends BaseActionActivity {
+
+
+}
